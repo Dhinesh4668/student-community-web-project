ba53fb4c5ceec5bb3cc321d94d8c9e91102af882
Dhinesh4668 <dhinesh4668@gmail.com>
2023-08-28 11:30:32 +0530

    dashbord system update component

--- client/src/app/Screens/HomeScreen/Dashbord.jsx
+++ client/src/app/Screens/HomeScreen/Dashbord.jsx
@@ -1,52 +1,61 @@
 import React from "react";
-import { Container, Typography, Card, CardContent, Grid } from "@mui/material";
+import { Container, Typography, Card, CardContent, Grid, CardActions, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Dashbord() {
-  return (
-    <Container maxWidth="lg" sx={{ marginTop: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        User Dashboard
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Profile
-              </Typography>
-              <Typography variant="body2">
-                View and update your profile information.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Orders
-              </Typography>
-              <Typography variant="body2">
-                Check your recent orders and order history.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Settings
-              </Typography>
-              <Typography variant="body2">
-                Manage your account settings and preferences.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    const navigate = useNavigate();
+    return (
+        <Container maxWidth="lg" sx={{ marginTop: 4 }}>
+            <Typography variant="h4" gutterBottom>
+                Welcome TO our Student StudyPlace
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                Profile
+                            </Typography>
+                            <Typography variant="body2">
+                                View and update your profile information.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                Notes Taking
+                            </Typography>
+                            <Typography variant="body2">
+                                Check your Add your notes and Acces the anywhare.
+                                <CardActions>
+                                    <Button size="medium" onClick={navigate('/notes')}>Click To OPEN</Button>
+                                </CardActions>
+                            </Typography>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                Meeting System
+                            </Typography>
+                            <Typography variant="body2">
+                            The student online meeting system facilitates seamless virtual collaboration among students.<br></br> It enables interactive discussions, project planning, and knowledge sharing.
+                            </Typography>
+                            <CardActions>
+                                <Button onClick={navigate('/meet')}>Click TO open</Button>
+                            </CardActions>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default Dashbord;
